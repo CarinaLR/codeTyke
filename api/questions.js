13fbd5031c510c6c77a66f17d927a6dc0b9e4b3d
CarinaLR <carinalopez@Carinas-MacBook-Pro.local>
2020-03-31 13:22:24 -0400

    adding api routers

--- api/questions.js
+++ api/questions.js
@@ -5,6 +5,11 @@ const models = require("../models");
 //new structure
 const Question = models.Question;
 
+//three ways of passing in info
+// - req.body - request
+// - req.params - path variable
+// - req.query - query string
+
 // middleware that is specific to this router
 //async functions handle promises.
 router.get("/", async (req, res, next) => {
@@ -14,6 +19,77 @@ router.get("/", async (req, res, next) => {
       include: ["options"]
     });
     res.status(200).res.json(questions);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    const { body, instructions, options } = req.body;
+    if (body && instructions) {
+      const newQuestion = await Question.create({
+        body,
+        instructions
+      });
+
+      res.status(200).json(newQuestion);
+    } else {
+      res.sendStatus(400);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const question = await Question.findByPk(id);
+    if (question) {
+      res.status(200).json(question);
+    } else {
+      res.sendStatus(400);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const questionToUpdate = await Question.findByPk(id);
+    if (questionToUpdate) {
+      const { body, instructions } = req.body;
+      const updatedQuestion = await questionToUpdate.update({
+        body,
+        instructions
+      });
+      res.status(200).json(updatedQuestion);
+    } else {
+      res.sendStatus(400);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    // another method:
+    // Question.destroy({
+    //   where: { id }
+    // });
+    // res.status(200).json({ status: "Deleted successfully" });
+    const questionToDelete = await Question.findByPk(id);
+    if (questionToDelete) {
+      const deletedQuestion = await questionToDelete.destroy();
+      res.status(200).json(deletedQuestion);
+    } else {
+      res.sendStatus(400);
+    }
   } catch (error) {
     next(error);
   }
