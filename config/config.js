bde8f7047c1279ef6a6a4832910e034f354bef49
CarinaLR <carinalopez@Carinas-MacBook-Pro.local>
2020-04-10 20:48:17 -0400

    fixing bug

--- config/config.js
+++ config/config.js
@@ -3,23 +3,23 @@ require("dotenv").config();
 module.exports = {
   development: {
     username: process.env.DB_USERNAME,
-    password: process.env.PASSWORD,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOSTNAME,
-    dialect: "mysql"
+    dialect: "mysql",
   },
   test: {
     username: "root",
     password: null,
     database: "database_test",
     host: "127.0.0.1",
-    dialect: "mysql"
+    dialect: "mysql",
   },
   production: {
     username: process.env.DB_USERNAME,
-    password: process.env.PASSWORD,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOSTNAME,
-    dialect: "mysql"
-  }
+    dialect: "mysql",
+  },
 };
